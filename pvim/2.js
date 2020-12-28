a7638a305a17991ee88cf3949eb95d1cf079c738
Sammy <sumezawa@protonmail.com>
2020-12-28 15:02:59 -0800

    2) .

---
+++ pvim/2.js
@@ -0,0 +1,11 @@
+var a = 1
+var b = 'a'
+var c = a + b
+
+// A;<Esc> (go to end, add a semicolon, return to NORMAL)
+// j. (go down 1 line, repeat A;<Esc>)
+
+var a = 1;
+var b = 'a';
+var c = a + b;
+
