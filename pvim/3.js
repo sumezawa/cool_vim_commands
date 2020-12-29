81ea555dfa8fbbda87ccf1f2d5d205d9a92cfdad
Sammy <sumezawa@protonmail.com>
2020-12-28 16:47:10 -0800

    ; command: perform last search

--- pvim/3.js
+++ pvim/3.js
@@ -1,4 +1,12 @@
 var a = "method("+argument1+","+argument2+")";
 
-// f+
-var a = "method("+argument1+","+argument2+")";
+// do: add space and declutter the line
+
+// f+ (find the first instance of '+' in the line
+// s +  (delete one to the right (so deletes '+' ; then add ' + ')
+// <Esc>
+
+// ; (repeat the last search, i.e. performs f+
+// . (repeat the last command, i.e. performs s + <Esc>
+
+var a = "method(" + argument1 + "," + argument2 + ")";
