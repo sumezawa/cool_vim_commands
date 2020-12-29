d23b2a1bf5d47bb8444281846bfa0e2d68ed83d6
Sammy <sumezawa@protonmail.com>
2020-12-28 16:48:54 -0800

    (.) repeat last NORMAL

--- pvim/2.js
+++ pvim/2.js
@@ -4,10 +4,12 @@ var c = a + b
 
 // do: add a semicolon to three lines (not range based)
 
-// A;<Esc> (go to end, add a semicolon, return to NORMAL)
-// j. (go down 1 line, repeat A;<Esc>)
+// A; (go to end, add a semicolon)
+// <Esc> (return to normal)
+
+// j (go down 1 line)
+// . (repeat last command, i.e. A;<Esc>)
 
 var a = 1;
 var b = 'a';
 var c = a + b;
-
