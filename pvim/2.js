b581a0e0c76ad1f090642631785ef72216b3986b
Sammy <sumezawa@protonmail.com>
2020-12-28 16:38:42 -0800

    . command

--- pvim/2.js
+++ pvim/2.js
@@ -2,6 +2,8 @@ var a = 1
 var b = 'a'
 var c = a + b
 
+// do: add a semicolon to three lines (not range based)
+
 // A;<Esc> (go to end, add a semicolon, return to NORMAL)
 // j. (go down 1 line, repeat A;<Esc>)
 
