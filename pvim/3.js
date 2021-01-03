a6841756a3a05e8a939eeb27377e949851058794
Sammy <sumezawa@protonmail.com>
2021-01-03 11:07:58 -0800

    (;) repeat last search

--- pvim/3.js
+++ pvim/3.js
@@ -2,7 +2,7 @@ var a = "method("+argument1+","+argument2+")";
 
 // do: add space and declutter the line
 
-// f+ (find the first instance of '+' in the line
+// f+ (find, on this line, the first instance of '+')
 // s +  (delete one to the right (so deletes '+' ; then add ' + ')
 // <Esc>
 
