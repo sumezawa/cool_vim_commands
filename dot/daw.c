84cc580c9833d68e899370da2b0c1656b424fcf2
Sammy <sumezawa@protonmail.com>
2021-01-09 13:49:05 -0800

    choose the command to repeat with .

---
+++ dot/daw.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("the best delete-a-word is the one that is one 'command'");
+    // daw, aka delete-a-word
+    
+    // What is 'aw'? 
+    // :help aw
+    // aw is a 'text object', explained later...
+    // daw tells Vim to delete a word, including preceding white space
+    
+    // anyways daw is the de facto best deletion command because it is 'one' command
+    // it is all under one normal-mode sequence, which means it can be replaced with .
+   
+    return 0;
+}
