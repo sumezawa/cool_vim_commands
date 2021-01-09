b11335ac9c9362314f9876173f1866861359006c
Sammy <sumezawa@protonmail.com>
2021-01-09 13:29:15 -0800

    (*) search this keyword

---
+++ dot/4.hs
@@ -0,0 +1,5 @@
+-- sample text, with three 'this':
+-- we want to delete this this or this
+
+-- What (*) does: search the keyword cursor is pointing to
+-- What (*) eliminates: needing to type your search pattern
