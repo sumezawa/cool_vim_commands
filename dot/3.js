e59229e4382eeca1be082fe76247f0215e65e195
Sammy <sumezawa@protonmail.com>
2021-01-09 13:28:39 -0800

    (;) repeat last search

---
+++ dot/3.js
@@ -0,0 +1,12 @@
+var a = "method("+argument1+","+argument2+")";
+
+// do: add space and declutter the line
+
+// f+ (find, on this line, the first instance of '+')
+// s +  (delete one to the right (so deletes '+' ; then add ' + ')
+// <Esc>
+
+// ; (repeat the last search, i.e. performs f+)
+// . (repeat the last command, i.e. performs s + <Esc>)
+
+var a = "method(" + argument1 + "," + argument2 + ")";
